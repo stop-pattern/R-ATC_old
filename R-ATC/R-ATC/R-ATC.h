9fd8d3eacc2acfbba0b4413793f9848f7a075dc3
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-08-13 20:28:59 +0900

    [change] split patterns and limits

--- R-ATC/R-ATC/R-ATC.h
+++ R-ATC/R-ATC/R-ATC.h
@@ -11,31 +11,48 @@ class c_R_ATC {
 private:
 	class Pattern {
 	public:
-		double Limit;	//制限速度[km/h] <=ATC現示値(=P_Speed
-		double target;	//停止限界[m] <=距離程
-		double StopLimit;	//停止限界残距離[m] <=毎フレーム更新値
-		double target_Speed;	//目標速度[km/h] <=入力値(固定)
-		double target_Location;	//目標距離程[m] <=入力値(固定)
-		double P_Speed;	//P接近速度
-		double B_Speed;	//B動作速度
-		double E_Speed;	//EB動作速度
-		double P_Location;	//P接近距離
-		double B_Location;	//B動作距離
-		double E_Location;	//EB動作距離
+		//double Limit;	//制限速度[km/h] <=ATC現示値(=P_Speed
+		double target = 0;	//停止限界[m] <=距離程
+		double StopLimit = 0;	//停止限界残距離[m] <=毎フレーム更新値
+		double target_Speed = 0;	//目標速度[km/h] <=入力値(固定)
+		double target_Location = DBL_MAX;	//目標距離程[m] <=入力値(固定)
+		double P_Speed = DBL_MAX;	//P接近速度
+		double B_Speed = DBL_MAX;	//B動作速度
+		double E_Speed = DBL_MAX;	//EB動作速度
+		double P_Location = DBL_MAX;	//P接近距離
+		//double B_Location;	//B動作距離
+		//double E_Location;	//EB動作距離
 		Pattern(double, double, double);
-		void calc(State, int *, int *);
-		void out(State, int *, int *);
-		bool useage;	//線形回帰判別
-		double jadge(void);	//P制御採用判定
+		int calc(State, int*, int*);
+		void out(State, int*, int*);
 		void SetBeaconData(int, int);
 	private:
-		double param;	//P制御採用判定用
 		double P_deceleration;	//P接近減速定数
 		double B_deceleration;	//B動作減速定数
 		double E_deceleration;	//EB動作減速定数
-		double a;	//先行計算用線形回帰パラメーター
-		double b;	//先行計算用線形回帰パラメーター
-		double c;	//先行計算用線形回帰パラメーター
+	};
+
+	class Limit {
+	public:
+		//double Limit;	//制限速度[km/h] <=ATC現示値(=P_Speed
+		double target = 0;	//停止限界[m] <=距離程
+		double StopLimit = 0;	//停止限界残距離[m] <=毎フレーム更新値
+		double target_Speed = 0;	//目標速度[km/h] <=入力値(固定)
+		double target_Location = DBL_MAX;	//目標距離程[m] <=入力値(固定)
+		double P_Speed = DBL_MAX;	//P接近速度
+		double B_Speed = DBL_MAX;	//B動作速度
+		double E_Speed = DBL_MAX;	//EB動作速度
+		double P_Location = DBL_MAX;	//P接近距離
+		//double B_Location;	//B動作距離
+		//double E_Location;	//EB動作距離
+		Pattern(double, double, double);
+		int calc(State, int*, int*);
+		void out(State, int*, int*);
+		void SetBeaconData(int, int);
+	private:
+		double P_deceleration;	//P接近減速定数
+		double B_deceleration;	//B動作減速定数
+		double E_deceleration;	//EB動作減速定数
 	};
 
 public:
@@ -51,36 +68,43 @@ public:
 		Step2,	//防護2段パターン
 		Crossing,	//踏切防護P
 		Route,	//路線依存P（曲線速度制限）
-		pattern_number	//P総数
+		number	//P総数
+	};
+	enum limit_name {
+		PreTrain,	//先行列車連動P
+		Step2,	//防護2段パターン
+		Crossing,	//踏切防護P
+		Route,	//路線依存P（曲線速度制限）
+		number	//P総数
 	};
 
 
 	void Load();	//consractor
-	void Status(State, int *, int *);	//ATC状態管理
-	void Interpolation();	//パラメーター算出
-	void Control(State, int *, int *);	//ATC制御
-	bool Update(State, Pattern);	//P更新・判定
-	void SetOut(void);	//出力値設定
+	void Status(State, int*, int*);	//ATC状態管理
+	void Interpolation();	//先行列車位置線形回帰
+	void Control(State, int*, int*);	//ATC制御
+	bool Update(State, c_R_ATC::Pattern);	//P更新・判定
 
 
-	Pattern* patterns[pattern_name::pattern_number];
+	Pattern* patterns[pattern_name::number];	//照査速度パターン制御
+	Limit* limits[limit_name::number];	//過走限界計算
 
 	int stat;	//ATCstatus
 
-	bool P;	//P接近
-	bool Bell;	//ATCベル
+	//bool P;	//P接近
+	//bool Bell;	//ATCベル
 
 	double StopLimit;	//停止限界残距離
 
-	double Limit;	//制限速度[km/h]
-	double Location;	//過走限界[m]
+	//double Limit;	//制限速度[km/h]
+	//double Location;	//過走限界[m]
 
 
-	double target;	//目標
+	//double target;	//目標
 	double pattern_speed[2];	//P接近速度
 	//double pattern_brake;	//���e�ō����x
-	double notice_dist;	//P接近距離
-	double brake_speed[11];	//B開始速度記録
+	//double notice_dist;	//P接近距離
+	//double brake_speed[11];	//B開始速度記録
 
 	//先行列車
 	std::vector<int> PreTrain_Time;	//時刻
