0c12ee7ba1fddc3b0491cb43d281050cbcd61c9d
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-09-26 23:46:00 +0900

    [fix]ドア依存マスコン制御

--- R-ATC/source/dllmain.cpp
+++ R-ATC/source/dllmain.cpp
@@ -65,7 +65,7 @@ DE void SC Initialize(int b) {
 }
 DE Hand SC Elapse(State S, int * p, int * s)
 {
-	if (door) handle.P = manual.P;
+	if (!door) handle.P = manual.P;
 	else handle.P = 0;
 	handle.B = manual.B;
 	handle.R = manual.R;
